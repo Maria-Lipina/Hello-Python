ef5017f12e3ac574ef0c621e57f34b8db35c37e0
Maria Lipina <m.o.lip714@gmail.com>
2022-01-21 22:50:13 +0300

    randomizer a bit + task 0

--- randomizer.py
+++ randomizer.py
@@ -3,4 +3,3 @@ import random
 print(random.randint(0, 20))
 print(random.randrange(100))
 print(random.randrange(0, 15, 3)) #This fixes the problem with randint() which includes the endpoint; in Python this is usually not what you want.
-
