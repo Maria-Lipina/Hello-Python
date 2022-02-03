8ac0bfbd0805febe132a5f146f58c065bd0cf82f
Maria Lipina <m.o.lip714@gmail.com>
2022-02-03 20:23:52 +0300

    done

--- 15.py
+++ 15.py
@@ -1,1 +1,6 @@
+import math
 print("15. Написать программу получающую набор произведений чисел от 1 до N. \n Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]")
+
+n = 7
+result = [math.factorial(i) for i in range(1, n+1)]
+print (result) 
