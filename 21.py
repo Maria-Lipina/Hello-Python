7b46495cd8ece9711274127af9379bc1bbce647c
Maria Lipina <m.o.lip714@gmail.com>
2022-02-07 20:45:49 +0300

    list comprehension

--- 21.py
+++ 21.py
@@ -1,4 +1,4 @@
-print("Определить, позицию второго вхождения строки в списке либо сообщить, что её нет.")
+print("21. Определить, позицию второго вхождения строки в списке либо сообщить, что её нет.")
 """Примеры
 список: ["qwe", "asd", "zxc", "qwe", "ertqwe"], ищем: "qwe", ответ: 3
 список: ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"], ищем: "йцу", ответ: 5
@@ -10,13 +10,10 @@ source = ['ORz6', 'ORz6', '368G', 'GD4,oO5']
 source1 = ['4no', 'A3s', '5vj', '4no', '5vj', 'x4n']
 
 def positions (source, elem):
-    pos = []
-    for i in range(len(source)):
-        if source[i] == elem:
-            pos.append(i)
+    pos = [i for i in range(len(source)) if source[i] == elem]
     return pos
 
-poss = positions(source1, '4no')
+poss = positions(source1, 'x4n')
 if len(poss) >=2: print(poss[1])
 else: print(-1)
 
