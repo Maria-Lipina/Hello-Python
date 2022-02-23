f4d646bd4483775646df1c828e009c3a75e70e7c
Maria Lipina <m.o.lip714@gmail.com>
2022-02-23 11:50:59 +0300

    experiment

---
+++ New _list/28 extlib/test.py
@@ -0,0 +1,4 @@
+from equation import quadratic_equation as qe
+roots = qe.QuadraticEquation(5, 3, -1)
+print(roots)
+#NB: То есть я, получается, могу для каких угодно задачек здесь скачивать внешние библиотеки, они 1) установятся в NL28\.extlib(скорее всего, но надо проверить) 2) они будут запускаться из папки, которая относится не к текущему подпроекту (Seminar tasks\New List), а к другому подпроекту (Seminar tasks\NL28), при условии, что все они входят в один проект (Seminar tasks)
