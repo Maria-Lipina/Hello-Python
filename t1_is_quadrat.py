5f39646f9f1f9cf042f8c5787ccfcb4b32efc05d
Maria Lipina <m.o.lip714@gmail.com>
2022-10-24 17:41:28 +0300

    задачи 1-3 добавлены и их текст в README

---
+++ t1_is_quadrat.py
@@ -0,0 +1,4 @@
+def is_quadrat (num1, num2):
+    return num1 == num2**2
+
+print(is_quadrat(5, 2))
---
+++ t2_find_max.py
@@ -0,0 +1,9 @@
+
+import random
+
+def find_max(li):
+    return max(li)
+
+li = [random.randrange(-10, 11) for i in range(5)]
+print(li)
+print(find_max(range(0, 10)))
